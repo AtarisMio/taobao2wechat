d44ec5136fe12096aeb72825bef0ada80fb82c00
Ataris <0w0@bigleg.tv>
2017-07-03 01:22:37 +0800

    fix

--- config/default.js
+++ config/default.js
@@ -3,6 +3,5 @@ module.exports = {
     domain: 'default.example.com',
     copyright: 'Copyright &copy;2017-2020 <a href="www.watchit.net.cn">Ataris</a> 技术提供',
     recordcode: null,
-    domain: 'http://127.0.0.1:3000',
     port: 3000
 }
