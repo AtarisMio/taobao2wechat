748dc9170b4faebf6a393ddddc3269c339e44d72
Ataris <0w0@bigleg.tv>
2017-07-03 02:09:22 +0800

    fix

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongodb: 'mongodb://localhost:27017/taobao2wechat',
     domain: 'default.example.com',
-    copyright: 'Copyright &copy;2017-2020 <a href="www.watchit.net.cn">Ataris</a> 技术提供',
+    copyright: 'Copyright &copy;2017-2020 <a href="http://www.watchit.net.cn">Ataris</a> 技术提供',
     recordcode: null,
     port: 3000
 }
